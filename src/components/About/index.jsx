4eb324ae76aceddf13e24074ba3d7f47ceea572c
Matthew Zmija <matthewzmija@Monicas-MacBook-Air.local>
2023-07-25 20:03:22 -0400

    add about me section

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React from "react";
+import avatar from "../../assets/small/portraits/3.jpg";
 
 function About() {
   return (
     <section className="my-5">
       <div className="my-2">
         <div className="profile-img my-5">
-        <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+          <img src={avatar} alt="avatar"></img>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
+          Hello everyone, I'm Matthew Zmija! I am currently employed as an
+          Account Manager at a startup software company. While I've enjoyed my
+          time in this role, I've recently graduated from Toronto Metropolitan
+          University in 2022 with a Bachelor of Commerce degree. Having just
+          started my career, I discovered a deep passion for coding and web
+          development, which has led me to make an exciting decision to shift my
+          career path and explore this new territory further.
         </p>
         <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
+          Currently, I'm pursuing my coding aspirations by enrolling in the
+          prestigious coding bootcamp at the University of Toronto. This
+          experience has been nothing short of transformative as I immerse
+          myself in the world of coding, learning the latest technologies and
+          programming languages to create captivating web experiences.
+        </p>
+        <p>
+          I am eager to take my newly acquired skills and knowledge and apply
+          them to the real world, transforming my passion for coding into a
+          fulfilling and successful career!
         </p>
       </div>
     </section>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-
-  // Replace links with social media profiles
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/matthewzmija",
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/matthewzmija/",
     },
     {
       name: "fab fa-stack-overflow",
-      link: "https://stackoverflow.com/"
-    }
-  ]
+      link: "https://stackoverflow.com/",
+    },
+  ];
 
   return (
     <footer className="flex-row px-1">
-      {icons.map(icon =>
-      (
-        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
-      )
-        )}
+      {icons.map((icon) => (
+        <a
+          href={icon.link}
+          key={icon.name}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className={icon.name}></i>
+        </a>
+      ))}
     </footer>
   );
 }
