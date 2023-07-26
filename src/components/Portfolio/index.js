ef380fbd20fa7e2976e1e0dd2f43707b866a86c7
Matthew Zmija <matthewzmija@Monicas-MacBook-Air.local>
2023-07-25 21:08:14 -0400

    ensure links work

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -12,26 +12,26 @@ function Portfolio() {
     {
       name: "movie-review",
       description: "Node.js/Javascript",
-      link: "https://github.com",
-      repo: "https://github.com",
+      link: "https://movie-reviews-2021.herokuapp.com/",
+      repo: "https://github.com/DanialDevelops/movie-review",
     },
     {
       name: "code-refactor",
       description: "HTML/CSS",
       link: "https://github.com",
-      repo: "https://github.com",
+      repo: "https://github.com/matthewzmija/CSS-HTML-Accessibility",
     },
     {
       name: "text-editor",
       description: "PWA/IndexedDB/Service Workers",
-      link: "https://github.com",
-      repo: "https://github.com",
+      link: "https://limitless-cove-61311-2c78e642b2d2.herokuapp.com/",
+      repo: "https://github.com/matthewzmija/PWA-Text-Editor",
     },
     {
       name: "password-generator",
       description: "HTML/CSS/JavaScript",
-      link: "https://github.com",
-      repo: "https://github.com",
+      link: "https://matthewzmija.github.io/JavaScript-Password-Generator/",
+      repo: "https://github.com/matthewzmija/JavaScript-Password-Generator",
     },
     {
       name: "timed-quiz",
